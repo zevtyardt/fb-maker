225c33501c059af13aa7435c40bfb96f2c3afe8c
zevtyardt <apklover76@gmail.com>
2019-06-30 13:45:22 +0700

    update v2: kebanyakan redirect jadi error: wkwk: add proxy support: remove bs4: sip ok, auto take over

--- fb-maker.py
+++ fb-maker.py
@@ -51,7 +51,7 @@ class create:
         pwd = res['results'][0]['login']['password']
         return {
             'username':  res['results'][0]['login']['username'],
-            'password':  pwd + '0000' if len(pwd) < 6 else pwd,
+            'password':  pwd + '-zvtyrdt.id' if len(pwd) < 6 else pwd,
             'firstname': res['results'][0]['name']['first'],
             'lastname':  res['results'][0]['name']['last'],
             'gender':    '1' if res['results'][0]['gender'] == 'female' else '2',
@@ -88,18 +88,43 @@ class create:
             self.br.select_form(nr=0)
             self.br.submit()
 
+        gagal = re.findall(r'id="registration-error"><div class="bl">(.+?)<', self.br.response().read())
+        if gagal:
+            logging.error(gagal[0])
+            return False
         return True
 
     def _check_email_fb(self, email):
-        r = requests.post('https://mbasic.facebook.com/login/identify/?ctx=recover',
-                data={ 'email': email }
-            )
+        self.br.open('https://mbasic.facebook.com/login/identify')
+        self.br._factory.is_html = True
+        self.br.select_form(nr=0)
+        self.br.form['email'] = email
+        self.br.submit()
 
-        if "recover_method" not in r.text:
-            logging.error('registered email!')
+        if "recover_method" in self.br.response().read():
+            logging.info("take overing account")
+            self.br._factory.is_html = True
+            self.br.select_form(nr=0)
+            self.br.form['recover_method'] = ['send_email']
+            self.br.submit()
             return False
 
         return True
+
+    def _submit_code(self, code):
+        self.br._factory.is_html = True
+        self.br.select_form(nr=0)
+        self.br.form['n'] = code
+        self.br.submit()
+
+    def _change_password(self):
+        data = self._get_info_account()
+        self._password = data['password']
+
+        self.br._factory.is_html = True
+        self.br.select_form(nr=0)
+        self.br.form['password_new'] = self._password
+        self.br.submit()
 
     # mail
     def _open_temp_mail(self):
@@ -136,17 +161,21 @@ class create:
                     logging.info('obtained email: %s', self._mail)
                     if self._check_email_fb(self._mail):
                         if self._create_account_facebook(self._mail):
-                            logging.info('waiting for incoming email')
                             email_found = True
+                    else:
+                        logging.info('waiting for incoming email')
+                        code = self._read_message(res_em)
+                        if code:
+                            self._submit_code(code)
+
                 if max_ == 10:
                     logging.error('no response !')
                     break
                 if check and email_found:
-                    if self._read_message(res_em):
-                        self.create_total += 1
-                        logging.info('account created:\n\t   email: %s\n\t   password: %s', self._mail, self._password)
-                        self._save_to_file(self._mail, self._password)
-                        check = False
+                    self.create_total += 1
+                    logging.info('account created:\n\t   email: %s\n\t   password: %s', self._mail, self._password)
+                    self._save_to_file(self._mail, self._password)
+                    check = False
                     max_ += 1
                 else: break
 
@@ -170,7 +199,7 @@ if __name__ == '__main__':
             create()
         except KeyboardInterrupt:
             logging.error('user interrupt..\n')
-        except Exception as exc:
-            logging.critical(str(exc) + '\n')
+#        except Exception as exc:
+ #           logging.critical(str(exc) + '\n')
     else:
         parse.print_help()
